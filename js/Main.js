1b2f7856263993a275e32b68555f7bd90fea0232
JohnHito <johnjairo988@gmail.com>
2024-10-06 14:32:54 -0600

    a

--- js/Main.js
+++ js/Main.js
@@ -1,25 +1,23 @@
-// Game configuration
+//Import de clases
 import Player from '/js/Player.js';
-//import playerSheet from "/assets/sprites/player_sheet.png";
 
 class Main extends Phaser.Scene {
 
-
+    //Metodo constructor
     constructor() {
         super({ key: "Main" })
         this.player = null;
-        this.falling = false;
-        this.isHitGroundComplete = false;
-        this.isJumpComplete = false;
     }
     preload() {
 
+        //Se añade el pluguin del joystick
         let url = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js';
         this.load.plugin('rexvirtualjoystickplugin', url, true);
 
+        //Se precarga la imagen de las plataformas y el spritesheet del jugador
         this.load.image('ground', '/assets/sprites/ground.png');
-        let playerSheet = this.load.spritesheet("playerSheet", "/assets/sprites/player_sheet-min.png", {
-
+        this.load.spritesheet("playerSheet", "/assets/sprites/player_sheet-min.png", {
+            //Se mandan las dimensiones de la mascara para cada frame de las animaciones
             frameWidth: 270,
             frameHeight: 164,
         });
@@ -27,87 +25,10 @@ class Main extends Phaser.Scene {
 
 
     create() {
-
-        
-
-
-
-        const run = {
-            key: "run",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33] }),
-            frameRate: 16,
-            repeat: -1
-        }
-        const idle = {
-            key: "idle",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [66, 67, 68, 69, 70, 71] }),
-            frameRate: 16,
-            repeat: -1
-        }
-        const startJump = {
-            key: "startJump",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [44, 45, 46, 47, 48, 49] }),
-            frameRate: 16,
-            repeat: 0
-        }
-        const goingUp = {
-            key: "goingUp",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [51, 52, 53, 54] }),
-            frameRate: 16,
-            repeat: -1
-        }
-        const falling = {
-            key: "falling",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [56, 57, 58, 59] }),
-            frameRate: 16,
-            repeat: -1
-        }
-        const hitGround = {
-            key: "hitGround",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [60, 61, 62, 63, 64] }),
-            frameRate: 16,
-            repeat: 0
-        }
-        const attack = {
-            key: "attack",
-            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }),
-            frameRate: 16,
-            repeat: 0
-        }
-
-        this.anims.create(run)
-        this.anims.create(idle)
-        this.anims.create(startJump)
-        this.anims.create(goingUp)
-        this.anims.create(falling)
-        this.anims.create(hitGround)
-        this.anims.create(attack)
-
-        this.player = this.physics.add.sprite(100, 100, "playerSheet");
-
-        this.player.play("idle", true)
-
-        this.player.on('animationcomplete-hitGround', () => {
-            this.isHitGroundComplete = true; // Set flag to true when animation completes
-        });
-        this.player.on('animationcomplete-startJump', () => {
-            this.isJumpComplete = true; // Set flag to true when animation completes
-        });
-
-        //Align.scaleToGameW(this.player,0.15,this)
+        //Crea la detecicon de teclas
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.platform = this.physics.add.staticGroup();
-        this.platform2 = this.physics.add.staticGroup();
-        this.platform.create(100, 300, 'ground').refreshBody();  // Static platform
-        this.platform2.create(400, 200, 'ground').refreshBody();  // Static platform
-        // Add collision between the player and the static platform
-        this.physics.add.collider(this.player, this.platform);
-        this.physics.add.collider(this.player, this.platform2);
-
-        this.player.scaleX = 0.7;
-        this.player.scaleY = 0.7;
-
+        //Crea un joystick para moverse desde celular a partir de un pluguin
         this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
             x: 80,
             y: 280,
@@ -116,77 +37,44 @@ class Main extends Phaser.Scene {
             thumb: this.add.circle(0, 0, 15, 0xcccccc),
         });
         this.joystickCursors = this.joyStick.createCursorKeys();
+
+        //Crea a un nuevo jugar, y le manda la escena, cordenadas, el sprite shee, el controlador de teclas y el joystick
+        this.player = new Player(this, 40, 40, 'playerSheet', this.cursors, this.joystickCursors)
+
+        this.player.scaleX = 0.7;
+        this.player.scaleY = 0.7;
+
+        //Crea objectos vacios con fisica estatica      
+        this.platform = this.physics.add.staticGroup();
+        this.platform2 = this.physics.add.staticGroup();
+
+        //A los objetos anteriores los añade en una posicion y con una textura y los actualiza
+        this.platform.create(100, 300, 'ground').refreshBody();  
+        this.platform2.create(400, 200, 'ground').refreshBody();  
+
+        //Añade colisiones entre el jugador y las plataformas
+        this.physics.add.collider(this.player, this.platform);
+        this.physics.add.collider(this.player, this.platform2);
     }
 
-    message() {
-        this.player.play("run", true)
-    }
     update() {
         this.player.update();
-
-        this.player.setVelocityX(0); this
-
-
-        if (!this.player.body.blocked.down) {
-            if (this.player.body.velocity.y < 0) {
-                this.player.play("goingUp", true);
-            } else if (this.player.body.velocity.y > 0) {
-                this.player.play("falling", true);
-                this.falling = true;
-            }
-        } else if (this.falling) {
-            this.player.play("hitGround", true);
-            this.falling = false;
-            this.isHitGroundComplete = false;
-
-        } else if (this.player.body.velocity.x == 0 && this.isHitGroundComplete && !this.cursors.up.isDown && !this.cursors.down.isDown && !this.cursors.left.isDown && !this.cursors.right.isDown) {
-            this.player.play("idle", true);
-        }
-
-        if (this.cursors.up.isDown == true || this.joystickCursors.up.isDown) {
-            this.player.play("startJump", true);
-            if (this.isJumpComplete) {
-                this.player.setVelocityY(-300);
-                this.isJumpComplete = false;
-            }
-        }
-        if (this.cursors.right.isDown == true || this.joystickCursors.right.isDown) {
-            this.player.setVelocityX(300);
-            this.player.play("run", true);
-            this.player.flipX = false;
-        }
-        if (this.cursors.left.isDown == true || this.joystickCursors.left.isDown) {
-            this.player.setVelocityX(-300);
-            this.player.play("run", true);
-            this.player.flipX = true;
-        }
-
-        if (this.cursors.down.isDown == true || this.joystickCursors.down.isDown) {
-            this.player.play("attack", true);
-        }
-
-        if (this.player.body.velocity.y < 0) {
-            this.player.play("goingUp", true);
-        } else if (this.player.body.velocity.y > 0) {
-            this.player.play("falling", true);
-        }
     }
 
 }
 
-
-
-
+//Configuracion del proyecto
 const config = {
     type: Phaser.AUTO,
     width: 640,
     height: 360,
     backgroundColor: '#2a6c6d',
     scene: Main,
+    //Añade fisicas 
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 3000},
             debug: false
         }
     }
--- js/Player.js
+++ js/Player.js
@@ -1,13 +1,150 @@
-export default class Player extends Phaser.GameObjects.Sprite {
+export default class Player extends Phaser.Physics.Arcade.Sprite {
 
-    constructor(scene, y, x, key) {
+    constructor(scene, y, x, key, cursors, joystickCursors) {
         super(scene, y, x, key);
+        //Se añade a si mismo a la escena existente
         scene.add.existing(this);
 
-        this.speed = 2;
+        //Se añade a si mismo a las fisicas de la escena existente
+        scene.physics.add.existing(this);
+
+        //Crea variables locales que guarden las variables recibidas por parametro
+        this.cursors = cursors;
+        this.joystickCursors = joystickCursors;
+
+        //Variables para controlar animaciones
+        this.falling = false;
+        this.isHitGroundComplete = false;
+        this.isJumpComplete = true; 
+        this.inGround = false;
+        this.jumps = true; 
+
+        //Llama a la funcion para crear las animaciones
+        this.createAnimations();
+    }
+    createAnimations() {
+
+        //Crea variables constantes con una key, y con los frames necesairos desde el player sheet
+        const run = {
+            key: "run",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33] }),
+            frameRate: 16,
+            repeat: -1
+        }
+        const idle = {
+            key: "idle",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [66, 67, 68, 69, 70, 71] }),
+            frameRate: 16,
+            repeat: -1
+        }
+        const startJump = {
+            key: "startJump",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [44, 45, 46, 47, 48, 49] }),
+            frameRate: 16,
+            repeat: 0
+        }
+        const goingUp = {
+            key: "goingUp",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [51, 52, 53, 54] }),
+            frameRate: 16,
+            repeat: -1
+        }
+        const falling = {
+            key: "falling",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [56, 57, 58, 59] }),
+            frameRate: 16,
+            repeat: -1
+        }
+        const hitGround = {
+            key: "hitGround",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [60, 61, 62, 63, 64] }),
+            frameRate: 16,
+            repeat: 0
+        }
+        const attack = {
+            key: "attack",
+            frames: this.anims.generateFrameNumbers("playerSheet", { frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }),
+            frameRate: 16,
+            repeat: 0
+        }
+
+        //Utiliza funcciones del Phaser para crear las animaciones anteriormente definidas
+        this.anims.create(run)
+        this.anims.create(idle)
+        this.anims.create(startJump)
+        this.anims.create(goingUp)
+        this.anims.create(falling)
+        this.anims.create(hitGround)
+        this.anims.create(attack)
     }
 
-    update(){
-        this.x += this.speed;
+    update() {
+        //Reseta la velocidad en X, de lo conbtrario no se detiene
+        this.setVelocityX(0); this
+
+        //Detecta si la animaicon "hitGround" se completo
+        this.on('animationcomplete-hitGround', () => {
+            this.isHitGroundComplete = true; 
+        });
+
+        //Control si el jugador esta colisionando con algo
+        if (this.body.blocked.down) {
+            this.inGround = true;
+            this.jumps = true;
+        }else{
+            this.inGround = false;
+        }
+
+        //Control de animacion si el jugador esta saltando
+        if (!this.inGround) {
+            if (this.body.velocity.y < 0) {
+                this.play("goingUp", true);
+
+                //Control de animacion Si el jugador esta callendo
+            } else if (this.body.velocity.y > 0) {
+                this.play("falling", true);
+                this.falling = true;
+            }
+
+            //Animacion al caer 
+        } else if (this.falling) {
+            this.play("hitGround", true);
+            this.falling = false;
+            this.isHitGroundComplete = false;
+        }
+
+        //Controlar animacion idle
+        if (this.body.velocity.x == 0 && this.body.velocity.y == 0 && this.isHitGroundComplete && !this.cursors.up.isDown && !this.cursors.down.isDown && !this.cursors.left.isDown && !this.cursors.right.isDown) {
+            this.play("idle", true);
+        }
+
+        //Control para saltar
+        if (this.cursors.up.isDown == true && this.jumps>0 || this.joystickCursors.up.isDown && this.jump) {
+            this.setVelocityY(-800);
+            this.jumps = false;
+            console.log(this.jumps)
+        }
+
+        //Controles de movimiento y animacion de izquierda y derecha
+        if (this.cursors.right.isDown == true || this.joystickCursors.right.isDown && this.body.velocity.y != 0) {
+            this.setVelocityX(300);
+            this.flipX = false;
+            //Si no esta en el aire, se ejecuta la animacion de correr
+            if (this.body.velocity.y == 0) { this.play("run", true); }
+        }
+        if (this.cursors.left.isDown == true || this.joystickCursors.left.isDown) {
+            this.setVelocityX(-300);
+            this.flipX = true;
+            //Si no esta en el aire, se ejecuta la animacion de correr
+            if (this.body.velocity.y == 0) { this.play("run", true); }
+        }
+
+        //Animacion de ataque
+        if (this.cursors.down.isDown == true || this.joystickCursors.down.isDown) {
+            this.play("attack", true);
+        }
+
+
+
     }
 }
