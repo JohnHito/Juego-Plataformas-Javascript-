2a3b2e1c025f9ba10512fe023c9a24d92234067c
JohnHito <johnjairo988@gmail.com>
2026-04-15 21:43:36 -0600

    asdd

--- Juego-Plataformas/webpack.config.prod.js
+++ Juego-Plataformas/webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./Juego-Plataformas/index.html",
     }),
     new CopyPlugin({
       patterns: [
