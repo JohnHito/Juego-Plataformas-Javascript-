ee4089b17a48aa8123a47dd467efb7dc9db7f0df
JohnHito <johnjairo988@gmail.com>
2026-04-15 21:42:06 -0600

    dsfffsdf

--- Juego-Plataformas/webpack.config.prod.js
+++ Juego-Plataformas/webpack.config.prod.js
@@ -1,25 +1,24 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: "Juego-Plataformas/img", to: "img" },
+        { from: "Juego-Plataformas/css", to: "css" },
+        { from: "Juego-Plataformas/js/vendor", to: "js/vendor" },
+        { from: "Juego-Plataformas/icon.svg", to: "icon.svg" },
+        { from: "Juego-Plataformas/icon.ico", to: "favicon.ico" },
+        { from: "Juego-Plataformas/robots.txt", to: "robots.txt" },
+        { from: "Juego-Plataformas/icon.png", to: "icon.png" },
+        { from: "Juego-Plataformas/site.webmanifest", to: "site.webmanifest" },
       ],
     }),
   ],
